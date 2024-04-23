ee47f1bd04b8eb9da7cd04b199ca63b9f9270f78
Paulo Francesco Pacheco <pfpacheco@gmail.com>
2024-04-22 21:56:55 -0300

    inserting a new controller

--- controllers/cadastro_controller.py
+++ controllers/cadastro_controller.py
@@ -1,14 +1,12 @@
 from factory.factory import ServiceFactory
 from models.cadastro_model import CadastroHttpResponse
 
-from fastapi.exceptions import HTTPException
-
 
 class CadastroController:
 
     def __init__(self):
         self.response = None
 
-    async def add_cadastro(self, cadastro) -> HTTPException | CadastroHttpResponse:
+    async def add_cadastro(self, cadastro) -> CadastroHttpResponse:
         self.response = await ServiceFactory.get_service(name="cadastro").add_cadastro(cadastro=cadastro)
         return self.response
